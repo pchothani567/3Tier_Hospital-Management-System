dc0735997c5d79f3c3220f3789c3a8fbef22cc72
PRIYA CHOTHANI <149994260+pchothani567@users.noreply.github.com>
2025-04-03 10:37:11 +0530

    Create FeedbackFormDetailShow.cs

---
+++ Admin/FeedbackFormDetailShow.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace Project_Hospital.Admin
+{
+    public class FeedbackFormDetailShow
+    {
+        string s = ConfigurationManager.ConnectionStrings["db"].ConnectionString;
+
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        DataSet ds;
+
+        public void startcon()
+        {
+            con = new SqlConnection(s);
+            con.Open();
+        }
+
+        //Select...1.1
+        public DataSet filldata()
+        {
+            da = new SqlDataAdapter("select * from Reviews", con);
+            ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+
+        //Delete....1
+        public void delete(int id)
+        {         
+            cmd = new SqlCommand("delete from Reviews where Id=" + id, con);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
