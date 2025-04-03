1eced4345764de5b7a216c83186a97faf13a6c54
PRIYA CHOTHANI <149994260+pchothani567@users.noreply.github.com>
2025-04-03 11:29:12 +0530

    Create News.aspx.cs

---
+++ News.aspx.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project_Hospital
+{
+    public partial class News : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["user"] != null)
+            {
+                lbl_welcome.Text = "WELCOME USER  " + Session["user"];
+            }
+            else
+            {
+                Response.Redirect("Login_User.aspx");
+            }
+        }
+
+        protected void logout_btn_Click(object sender, EventArgs e)
+        {
+            Session.Clear();
+            Session.Abandon();
+            Response.Redirect("Login_User.aspx");
+        }
+    }
+}
