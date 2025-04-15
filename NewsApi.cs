53584d26a594f3077d088173976fedeff2bd2532
PRIYA CHOTHANI <149994260+pchothani567@users.noreply.github.com>
2025-04-15 21:31:21 +0530

    Create NewsApi.cs

---
+++ NewsApi.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_Hospital
+{
+    public class NewsApi
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string PublishDate { get; set; }
+        public string Description { get; set; }
+    }
+}
